8067b9f512736714efa2e5498604c0d7e31c580d
Emily <140002969+AngelBaee@users.noreply.github.com>
2024-04-14 01:45:26 +0500

    first commit

---
+++ default.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypo = np.array([3,8,1,10,5,7])
+
+plt.plot(ypo)
+plt.show()
---
+++ multiplep.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpo = np.array([1,2,6,8])
+ypo = np.array([3,8,1,10])
+
+plt.plot(xpo,ypo)
+plt.show()
