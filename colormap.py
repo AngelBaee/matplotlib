a36f1b619ad7e2ac7dd3121e75e33796fb0f31ec
Emily <140002969+AngelBaee@users.noreply.github.com>
2024-04-19 23:58:29 +0500

    Add files via upload

---
+++ colormap.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+kk = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+ll = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+colors = np.array([0,10,20,30,40,45,50,55,60,70,80,90,100])
+
+plt.scatter(kk,ll,c = colors, cmap = 'viridis')
+
+plt.colorbar()
+plt.show()
